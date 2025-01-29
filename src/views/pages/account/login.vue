d107850897224310c1dfae8d0d8260e15841648a
Jafari M. Akida <Jakida@tucode.co.ke>
2025-01-29 15:13:57 +0300

    updated login

--- src/views/pages/account/login.vue
+++ src/views/pages/account/login.vue
@@ -26,8 +26,8 @@
 </style>
 <script>
 import { required, email } from "vuelidate/lib/validators";
-import headerVue from "@/EstateAgency/components/header.vue";
-import footerVue from "@/EstateAgency/components/footer.vue";
+// import headerVue from "@/EstateAgency/components/header.vue";
+// import footerVue from "@/EstateAgency/components/footer.vue";
 
 import {
   authMethods,
@@ -38,8 +38,8 @@ import {
 
 export default {
   components: {
-    headerVue,
-    footerVue
+    // headerVue,
+    // footerVue
 },
   inject: ['mySpinner'],
 
@@ -125,7 +125,6 @@ export default {
 
 <template>
   <body>
-    <headerVue/>
     <div class="" style="">
     <!-- <BtnLoader/> -->
     <!-- <BTNSPINNER/> -->
@@ -271,7 +270,6 @@ export default {
             </div>
           </div>
         </div>
-        <footerVue></footerVue>
       </div>
     </div>
   </div>
